ef1fe5d9858508ac422a0546d3b985519ed446f2
Niles Turner <turnerniles@gmail.com>
2015-08-03 23:34:53 -0400

    gilt api working

--- server.js
+++ server.js
@@ -19,39 +19,60 @@ app.get('/', function(req, res){
 
   var apiKey = "e0f5b85f64d239a5945fede8a95a3ddce8d6e952cef546b371b3b7b3ca213468";
 
-  function saveCheck(err, data){
-    console.log('checking');
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
+  request.get({
+    url: 'https://app.cloudscrape.com/api/runs/{runId}/latest/result'
+    headers: {
+      X-CloudScrape-Access: a84e3ed9bfb1343c0a2c90d94,
+      X-CloudScrape-Account: 365db7d8-9186-4389-8c28-5666bf523010,
+      Accept: application/json,
+      Accept-Encoding: gzip,
+      Host: app.cloudscrape.com,
+      User-Agent: YourApp/1.0
     }
-  }
-
-  request.get("https://api.gilt.com/v1/sales/men/upcoming.json",
-  {apikey: 'e0f5b85f64d239a5945fede8a95a3ddce8d6e952cef546b371b3b7b3ca213468'},
-  function(error, response, body) {
-    var sales = body.sales;
-    console.log('hey');
+  },
+  function(error,response,body){
     console.log(body);
-    if(sales instanceof Array){
-      console.log('ho');
-        for(var i=0; i<sales.length;i++){
-          console.log('entering loop');
-          var beg = new Date(sales[i].begins);
-          console.log(beg);
-          var name = sales[i].name;
-          console.log(name);
-          var newSale = new Sale({
-                          title: sales[i].name,
-                          date: sales[i].begins
-                          });
-          newSale.save(saveCheck(err, data));
+  }
+  )
+
+
+  request.get({
+  url: 'https://api.gilt.com/v1/sales/men/upcoming.json',
+  headers: {
+          apikey: apiKey
+    }
+  },
+  function(error, response, body) {
+
+    function saveCheck(err, data){
+      // console.log('checking');
+      if (err) {
+        // console.log(err);
+      } else {
+        // console.log(data);
       }
     }
-    res.render('index.ejs', {data: sales});
-  });
 
+var bod = JSON.parse(body);
+var sales = bod.sales;
+
+console.log('bye');
+      if(sales instanceof Array){
+          for(var i=0; i<sales.length;i++){
+            var beg = new Date(sales[i].begins);
+            console.log(beg);
+            var name = sales[i].name;
+            console.log(name);
+            var newSale = new Sale({
+                            title: name,
+                            date: beg
+                            });
+            // newSale.save(function(error, data){saveCheck(err, data)
+            // });
+        }
+      }
+      res.render('index.ejs', {data: sales});
+  });
 })
 
 mongoose.connect(MONGOURI);
